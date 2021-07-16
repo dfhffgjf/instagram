d6609b2a570c3311b89558fdcff3b3fb7300ff2f
TitovMaximDPG <titovmaximdpg@gmail.com>
2021-07-16 18:22:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('KEK')
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '147593807478840',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v11.0'
+    });
+
+    FB.AppEvents.logPageView();
+
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
